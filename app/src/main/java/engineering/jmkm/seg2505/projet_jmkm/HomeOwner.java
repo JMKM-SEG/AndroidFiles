5c3569cf4d92316bb6a68d1f7b6aba8ad27d1c85
Kenza tita <kboud084@uottawa.ca>
2018-10-23 15:17:39 -0400

    Ajout de la classe HomeOwner et ServiceProvider

---
+++ app/src/main/java/engineering/jmkm/seg2505/projet_jmkm/HomeOwner.java
@@ -0,0 +1,13 @@
+package engineering.jmkm.seg2505.projet_jmkm;
+
+public class HomeOwner extends UserAccount {
+
+    public HomeOwner(String firstName, String lastName, String eMail,String phoneNumber, String userName,String nameOfCountry, String nameOfCity){
+        super(firstName,lastName, eMail,phoneNumber, userName,nameOfCountry, nameOfCity);
+
+    }
+    public void researchServiceProvider(){
+
+    }
+
+}
---
+++ app/src/main/java/engineering/jmkm/seg2505/projet_jmkm/ServiceProvider.java
@@ -0,0 +1,10 @@
+package engineering.jmkm.seg2505.projet_jmkm;
+
+
+
+public class ServiceProvider extends UserAccount {
+
+    public ServiceProvider(String firstName,String lastName, String eMail,String phoneNumber, String userName, String nameOfCountry, String nameOfCity){
+        super(firstName,lastName, eMail,phoneNumber, userName, nameOfCountry, nameOfCity);
+    }
+}
